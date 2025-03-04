04f1171c93bf945214f1b43a7be4166d7815ac1f
nadia12-lab <nadineshannel405@gmail.com>
2025-03-04 18:03:57 +0800

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 's3.amazonaws.com',
+          port: '',
+          pathname: '/my-bucket/**',
+          search: '',
+        },
+      ],
+    },
+  }
